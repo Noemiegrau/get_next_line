3cc91099e2a2ece68b2ce3e8b32cc5828e207d12
Noemie Grau <nograu@c3r5p5.42perpignan.fr>
2025-11-26 18:53:31 +0100

    headers added

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nograu <nograu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 18:05:33 by nograu            #+#    #+#             */
+/*   Updated: 2025/11/26 18:31:51 by nograu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -7,10 +19,10 @@ char	*get_next_line(int fd)
 //	static char	*stash;
 	size_t			bytes_read;
 
-	buffer = malloc(sizeof(char) * (5 + 1));
+	buffer = malloc(sizeof(char) * (3 + 1)); // change n
 	if (!buffer)
 		return (NULL);
-	bytes_read = read(fd, buffer, 5); // replace by nbytes ?
+	bytes_read = read(fd, buffer, 3); // replace by nbytes ?
 	if (bytes_read <= 0)
 		return (NULL);
 //	stash = calloc(sizeof(char) * bytes_read + 1);
@@ -28,15 +40,20 @@ char	*get_next_line(int fd)
 #include <stdio.h>
 #include <fcntl.h>
 
-int	main(void)
+int	main(void) // change n
 {
-	int	fd = open("example.txt", O_RDONLY);
-	int count = 0;
-	char *result = get_next_line(fd);
+	int	fd;
+	int count;
+	char *next_line = get_next_line(fd);
 
+	count = 0;
+	fd = open("example.txt", O_RDONLY);
+	
+	next_line = get_next_line(fd);
 	count++;
-	printf("[%d]:%s\n", count, result);
-	result = NULL;
+	printf("[%d]:%s\n", count, next_line);
+	next_line = NULL;
+	
 	close(fd);
 	return (0);
 }
